1e09f7beef335bfa9c9294fbfa62a383b0761dd8
Echamma <151067103+Echamma@users.noreply.github.com>
2024-01-10 09:18:58 -0600

    finished all I did

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Donebutton = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.Save = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(868, 90);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(390, 433);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.Click += new System.EventHandler(this.listBox2_Click);
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(991, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(868, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(46, 21);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "DEL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1043, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(443, 57);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(46, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "DEL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(443, 90);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(390, 433);
+            this.listBox1.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(187, 59);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(215, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // Donebutton
+            // 
+            this.Donebutton.Location = new System.Drawing.Point(12, 59);
+            this.Donebutton.Name = "Donebutton";
+            this.Donebutton.Size = new System.Drawing.Size(46, 21);
+            this.Donebutton.TabIndex = 11;
+            this.Donebutton.Text = "DONE";
+            this.Donebutton.UseVisualStyleBackColor = true;
+            this.Donebutton.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(135, 59);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(46, 20);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "ADD";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(12, 90);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(390, 433);
+            this.listBox3.TabIndex = 9;
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(1293, 90);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 13;
+            this.Save.Text = "SAVE";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1477, 846);
+            this.ClientSize = new System.Drawing.Size(1412, 521);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Donebutton);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button Donebutton;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button Save;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,218 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms.Design;
+using System.Runtime.InteropServices;
+using System.Xml;
+
 
 namespace OrganizationBoard
 {
+
     public partial class Form1 : Form
     {
-        public Form1()
+        public class csvValues
+        {
+            public string Todo { get; set; }
+            public string Done { get; set; }
+        }
+        string folderPath;
+        string[] filePaths;
+        public List<string> lstToDo=  new List<String>();
+        public List<string> lstDone= new List<String>();
+
+        //Writes to First Column (Todo)
+        public void csvWriterTodo(string toBeWritten, string path)
+        {
+                    string file = @path;
+                    List<string> csv = new List<string>() { toBeWritten + "," };
+                    File.AppendAllLines(file, csv);
+        }
+        
+        //Writes to Second Column (Done)
+        public void csvWriterDone(string toBeWritten, string path)
+        {
+            string file = @path;
+            List<string> csv = new List<string>() { "," + toBeWritten };
+            File.AppendAllLines(file, csv);
+        }
+        static public List<string> csvReader(string path, int position)
+        {
+            List<string> listToReturn = new List<string>();
+
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                // Assuming your CSV has two columns and you want to read the specified position
+                while (csv.Read())
+                {
+                    if(string.IsNullOrWhiteSpace(csv.GetField<string>(position))== false)
+                    listToReturn.Add(csv.GetField<string>(position));
+                }
+            }
+            return listToReturn;
+        }
+            public Form1()
         {
             InitializeComponent();
         }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            folderPath = @"D:\C#\OrganizationBoard\SavedCSV\";
+            filePaths = Directory.GetFiles(folderPath);
+            foreach (string filename in filePaths)
+            {
+                listBox2.Items.Add(Path.GetFileName(filename));
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            String file = @"D:\C#\OrganizationBoard\SavedCSV\" + textBox1.Text.ToString() + ".csv";
+
+            using (var writer = new StreamWriter(file)) 
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteHeader<csvValues>();
+                csv.NextRecord();
+            }
+            listBox2.Items.Clear();
+            folderPath = @"D:\C#\OrganizationBoard\SavedCSV\";
+            filePaths = Directory.GetFiles(folderPath);
+            foreach (string filename in filePaths)
+            {
+                listBox2.Items.Add(Path.GetFileName(filename));
+            }
+
+        }
+        private void listBox2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox3.Items.Clear();
+            if (listBox2.SelectedItem != null)
+            {
+                String file = @"D:\C#\OrganizationBoard\SavedCSV\" + listBox2.SelectedItem.ToString();
+                lstToDo = csvReader(file, 0);
+                lstDone = csvReader(file, 1);
+            }
+            foreach (string item in lstToDo)
+            {
+                listBox3.Items.Add(item);
+            }
+            foreach (string item in lstDone)
+            {
+                listBox1.Items.Add(item);
+            }
+
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+
+        {
+            listBox1.Items.Clear();
+            listBox3.Items.Clear();
+            if (listBox2.SelectedItem != null) { 
+            File.Delete(@"D:\C#\OrganizationBoard\SavedCSV\" + listBox2.SelectedItem.ToString());
+            }
+            listBox2.Items.Remove(listBox2.SelectedItem);
+            
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string path = @"D:\C#\OrganizationBoard\SavedCSV\" + listBox2.SelectedItem.ToString();
+
+            List<string> lines = File.ReadAllLines(path).ToList();
+            if (!string.IsNullOrWhiteSpace(listBox3.SelectedItem?.ToString()))
+            {
+                string stringToRemove = listBox3.SelectedItem.ToString();
+
+                // Assuming lines is a List<string>
+                lines = lines
+                    .Where(line =>
+                    {
+                        // Split the line into columns
+                        string[] columns = line.Split(',');
+
+                        // Check if the first column contains the string to remove
+                        return columns.Length > 0 && !columns[0].Contains(stringToRemove);
+                    })
+                    .ToList();
+
+                File.WriteAllLines(path, lines);
+            }
+            listBox1.Items.Add(listBox3.SelectedItem);
+            csvWriterDone(listBox3.SelectedItem.ToString(), path);
+            lstToDo.Remove(listBox3.SelectedItem.ToString());
+            listBox3.Items.Remove(listBox3.SelectedItem);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string path = @"D:\C#\OrganizationBoard\SavedCSV\" + listBox2.SelectedItem.ToString();
+            csvWriterTodo(textBox3.Text.ToString(), path);
+            listBox3.Items.Add(textBox3.Text);
+            lstToDo.Add(textBox3.Text);
+            
+                
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string path = @"D:\C#\OrganizationBoard\SavedCSV\" + listBox2.SelectedItem.ToString();
+            List<string> lines = File.ReadAllLines(path).ToList();
+            if (!string.IsNullOrWhiteSpace(listBox1.SelectedItem?.ToString()))
+            {
+                string stringToRemove = listBox1.SelectedItem.ToString();
+
+                // Assuming lines is a List<string>
+                lines = lines
+                    .Where(line =>
+                    {
+                        // Split the line into columns
+                        string[] columns = line.Split(',');
+
+                        // Check if the first column contains the string to remove
+                        return columns.Length > 0 && !columns[1].Contains(stringToRemove);
+                    })
+                    .ToList();
+
+                File.WriteAllLines(path, lines);
+            
+            }
+            lstDone.Remove(listBox1.SelectedItem.ToString());
+            listBox1.Items.Remove(listBox1.SelectedItem);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem != null)
+            {
+                String file = @"D:\C#\OrganizationBoard\SavedCSV\" + listBox2.SelectedItem.ToString();
+                lstToDo = csvReader(file, 0);
+                lstDone = csvReader(file, 1);
+            }
+            foreach (string item in lstToDo)
+            {
+                listBox3.Items.Add(item);
+            }
+            foreach (string item in lstDone)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
     }
 }
+
